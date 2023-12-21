0227f3c73c84a05de4a6629cb1bdc289223079a6
bonitoFotso <bonitofotso55@gmai.com>
2023-12-21 12:01:20 +0100

    login

--- src/sections/login/login-view.jsx
+++ src/sections/login/login-view.jsx
@@ -1,12 +1,11 @@
+/* eslint-disable no-unused-vars */
+import axios from 'axios';
 import { useState } from 'react';
 
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
 import Card from '@mui/material/Card';
 import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import Divider from '@mui/material/Divider';
-import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import LoadingButton from '@mui/lab/LoadingButton';
@@ -15,15 +14,22 @@ import InputAdornment from '@mui/material/InputAdornment';
 
 import { useRouter } from 'src/routes/hooks';
 
+import API_URL from 'src/config';
 import { bgGradient } from 'src/theme/css';
 
 import Logo from 'src/components/logo';
 import Iconify from 'src/components/iconify';
+import EmailField from 'src/components/form/EmailField';
+import PasswordField from 'src/components/form/passwd/PasswordField';
+
 
 // ----------------------------------------------------------------------
 
 export default function LoginView() {
   const theme = useTheme();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState({});
 
   const router = useRouter();
 
@@ -33,15 +39,66 @@ export default function LoginView() {
     router.push('/dashboard');
   };
 
+  const validateForm = () => {
+    let valid = true;
+    const newErrors = {};
+
+    // Validation d'e-mail
+    if (!email) {
+      newErrors.email = 'Email is required';
+      valid = false;
+    }
+
+    // Validation du mot de passe
+    if (!password) {
+      newErrors.password = 'Password is required';
+      valid = false;
+    }
+
+    setErrors(newErrors);
+    return valid;
+  };
+
+  const handleLogin = async () => {
+    if (validateForm()) {
+      try {
+        // Envoyer la requête de connexion
+        const response = await axios.post(`${API_URL}/categories/`, {
+          email,
+          password,
+        });
+
+        // Stocker les données utilisateur dans le stockage local (localStorage ou sessionStorage)
+        localStorage.setItem('user', JSON.stringify(response.data));
+
+        // Rediriger ou effectuer d'autres actions après la connexion réussie
+        router.push('/dashboard');
+      } catch (error) {
+        console.error('Error during login:', error);
+        // Gérer les erreurs de connexion
+      }
+    }
+  };
+
   const renderForm = (
     <>
       <Stack spacing={3}>
-        <TextField name="email" label="Email address" />
+      <EmailField
+          name="email"
+          label="Email address"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          error={errors.email}
+        />
 
-        <TextField
+        <PasswordField
           name="password"
           label="Password"
+          value={password}
           type={showPassword ? 'text' : 'password'}
+          onChange={(e) => setPassword(e.target.value)}
+          error={errors.password}
+          helperText={errors.password}
           InputProps={{
             endAdornment: (
               <InputAdornment position="end">
@@ -66,7 +123,7 @@ export default function LoginView() {
         type="submit"
         variant="contained"
         color="inherit"
-        onClick={handleClick}
+        onClick={handleLogin}
       >
         Login
       </LoadingButton>
@@ -99,52 +156,9 @@ export default function LoginView() {
             maxWidth: 420,
           }}
         >
-          <Typography variant="h4">Sign in to Minimal</Typography>
+          <Typography variant="h4">Sign in to Servitel</Typography>
 
-          <Typography variant="body2" sx={{ mt: 2, mb: 5 }}>
-            Don’t have an account?
-            <Link variant="subtitle2" sx={{ ml: 0.5 }}>
-              Get started
-            </Link>
-          </Typography>
-
-          <Stack direction="row" spacing={2}>
-            <Button
-              fullWidth
-              size="large"
-              color="inherit"
-              variant="outlined"
-              sx={{ borderColor: alpha(theme.palette.grey[500], 0.16) }}
-            >
-              <Iconify icon="eva:google-fill" color="#DF3E30" />
-            </Button>
-
-            <Button
-              fullWidth
-              size="large"
-              color="inherit"
-              variant="outlined"
-              sx={{ borderColor: alpha(theme.palette.grey[500], 0.16) }}
-            >
-              <Iconify icon="eva:facebook-fill" color="#1877F2" />
-            </Button>
-
-            <Button
-              fullWidth
-              size="large"
-              color="inherit"
-              variant="outlined"
-              sx={{ borderColor: alpha(theme.palette.grey[500], 0.16) }}
-            >
-              <Iconify icon="eva:twitter-fill" color="#1C9CEA" />
-            </Button>
-          </Stack>
-
-          <Divider sx={{ my: 3 }}>
-            <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-              OR
-            </Typography>
-          </Divider>
+          
 
           {renderForm}
         </Card>
